341fb15db02bd1a7afb8ed18f712e678857e2a18
IyadShobaki <iyadshobaki82@gmail.com>
2020-09-20 12:52:00 -0400

    Moved Secure Key to Configuration

--- TulipDataManager/Startup.cs
+++ TulipDataManager/Startup.cs
@@ -61,7 +61,7 @@ namespace TulipDataManager
                 JwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                     ValidateIssuer = false,
                     ValidateAudience = false,
                     ValidateLifetime = true,
